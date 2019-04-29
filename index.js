74f82c3ce4b904c2bad74f199f124800d6b2e6bc
picachuwu <picachuwu@e-formula.com>
2019-04-29 20:50:56 +0800

    send to 118 server

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
+
 const app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.raw({ inflate: true, limit: '100kb', type: 'text/plain' }));
 
-app.get('/', function(req, res){
+app.post('/', function (req, res) {
     console.log(req.ip);
     console.log(req.body);
     res.sendStatus(200);
-});
-app.post('/', function(req, res){
-    console.log(req.ip);
-    console.log(req.body);
-    res.sendStatus(200);
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: '118.24.143.101',
+        port: '8088',
+        path: '/unabiz',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(JSON.stringify(req.body))
+        }
+    };
+    // Set up the request
+    var post_req = http.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    post_req.write(JSON.stringify(req.body));
+    post_req.end();
 })
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
