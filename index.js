11b7cf9bb473d91919315134d5cd4817ec2462c5
picachuwu <picachuwu@e-formula.com>
2019-04-29 20:06:28 +0800

    add post

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', function(req, res){
     res.send("Hello");
 });
+app.post('/', function(req, res){
+    console.log(JSON.stringify(req));
+})
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
